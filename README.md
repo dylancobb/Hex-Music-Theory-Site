fbddc1a17f6465ad36e65062bdc7121a4637270b
Hex <hexmusictheory@protonmail.com>
2023-03-27 00:11:28 +0100

    feat: add menu animations + refactor code

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # TODO
-- switch to using window.matchMedia when checking for window resizes
-    - (https://webdevetc.com/blog/matchmedia-events-for-window-resizes/)
 - Tidy up / refactor styles.css and scripts.js
 - Finish index page
 - Make links direct somewhere sensible
--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
  ******************/
 
 let sideBar = false;
-let canToggle = true; // stop double toggling while menu is closing
+let canToggle = true; // block toggling while menu is currently closing
 const menu = document.getElementById("nav-desktop");
 const tinter = document.getElementById("tinter");
 
@@ -64,14 +64,23 @@ function closeMenu() {
 }
 
 // make sure screen resizes don't break sidebar functionality
-window.addEventListener('resize', function (event) {
-    // check the current width of the window
-    const width = window.innerWidth;
-    // make sure top-menu is reset
-    if (width > 768) {
-        for (let i = 0; i < dropdown.length; i++) {
-            closeDropdowns(i);
+const mediaQuery = '(max-width: 768px)';
+const mediaQueryList = window.matchMedia(mediaQuery);
+mediaQueryList.addEventListener('change', event => {
+    // mobile display mode
+    if (event.matches) {
+        mobileDropdownListeners();
+        // hide nav menu if shown
+        if (sideBar) {
+            menu.style.display = "flex";
+        } else {
+            menu.style.transitionDuration = "0.3s";
+            menu.style.display = "none";
+            tinter.style.display = "none";
         }
+    // desktop display road
+    } else {
+        closeDropdowns();
         const icon = document.getElementById("menu-icon");
         if (icon.classList.contains("change"))
             icon.classList.toggle("change");
@@ -80,88 +89,46 @@ window.addEventListener('resize', function (event) {
         menu.style.display = "flex";
         tinter.style.display = "none";
         sideBar = false;
-        // if the side menu was open, leave it open
-    } else if (width <= 768) {
-        mobileDropdownListeners();
-        if (sideBar) {
-            menu.style.display = "flex";
-        } else {
-            menu.style.transitionDuration = "0.3s";
-            menu.style.display = "none";
-            tinter.style.display = "none";
-        }
     }
-});
-
-/*****************
- * Project Cards *=============================================================
- *****************/
-
-// grab project card text
-const descriptions = document.querySelectorAll(".card-text");
-
-// hide project card text by default
-descriptions.forEach(element => {
-    element.style.display = "none";
-});
-
-// toggle project card displays
-function showHideDescription(textID, buttonID, ellipsisID) {
-    const text = document.querySelector(textID);
-    const button = document.querySelector(buttonID);
-    const ellipsis = document.querySelector(ellipsisID);
-
-    if (text.style.display === "none") {
-        text.style.display = "block";
-        button.innerHTML = "Show less";
-        ellipsis.style.display = "none";
-    } else {
-        text.style.display = "none";
-        button.innerHTML = "Show more";
-        ellipsis.style.display = "inline";
-    }
-}
+})
 
 const dropdown = document.getElementsByClassName("dropdown");
 
 function toggleDropdown() {
     const dropdownContent = this.querySelector(".dropdown-content");
-    dropdownContent.classList.toggle("active");
     const chevron = this.querySelector(".chevron");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
+    if (dropdownContent.style.maxHeight) {
+        dropdownContent.style.maxHeight = null;
         if (chevron.classList.contains("chevron-rotate"))
             chevron.classList.toggle("chevron-rotate");
     } else {
-        for (let i = 0; i < dropdown.length; i++) {
-            closeDropdowns(i);
-        }
-        dropdownContent.style.display = "block";
+        closeDropdowns();
+        dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
         if (!chevron.classList.contains("chevron-rotate"))
             chevron.classList.toggle("chevron-rotate");
     }
 }
 
-function closeDropdowns(i) {
-    const dropdownContent = dropdown[i].querySelector(".dropdown-content");
-    if (dropdownContent.classList.contains("active"))
-        dropdownContent.classList.toggle("active");
-    const chevron = dropdown[i].querySelector(".chevron");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
+function closeDropdowns() {
+    for (let i = 0; i < dropdown.length; i++) {
+        const dropdownContent = dropdown[i].querySelector(".dropdown-content");
+            dropdownContent.style.maxHeight = null;
+        const chevron = dropdown[i].querySelector(".chevron");
         if (chevron.classList.contains("chevron-rotate"))
             chevron.classList.toggle("chevron-rotate");
     }
 }
 
 function toggleHover() {
-    this.querySelector(".dropdown-content").style.display = "block";
+    const dropdownContent = this.querySelector(".dropdown-content");
+    dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
 }
 
 function closeHover() {
-    this.querySelector(".dropdown-content").style.display = "none";
+    this.querySelector(".dropdown-content").style.maxHeight = null;
 }
 
+// remove accordion listeners, add hover-dropdown listeners
 function desktopDropdownListeners() {
     for (let i = 0; i < dropdown.length; i++) {
         dropdown[i].removeEventListener("click", toggleDropdown);
@@ -172,6 +139,7 @@ function desktopDropdownListeners() {
     }
 }
 
+// remove hover-dropdown listeners, add accordion listeners
 function mobileDropdownListeners() {
     for (let i = 0; i < dropdown.length; i++) {
         // Add the click event listener for screens larger than 768 pixels
@@ -182,19 +150,8 @@ function mobileDropdownListeners() {
     }
 }
 
-for (let i = 0; i < dropdown.length; i++) {
-    // Remove the click event listener for screens smaller than 768 pixels
-    if (window.innerWidth < 768) {
-        dropdown[i].removeEventListener("click", toggleDropdown);
-        dropdown[i].removeEventListener("mouseenter", toggleHover);
-        dropdown[i].removeEventListener("mouseleave", closeHover);
-        dropdown[i].addEventListener("click", toggleDropdown);
-    } else {
-        // Add the click event listener for screens larger than 768 pixels
-        dropdown[i].removeEventListener("click", toggleDropdown);
-        dropdown[i].removeEventListener("mouseenter", toggleHover);
-        dropdown[i].removeEventListener("mouseleave", closeHover);
-        dropdown[i].addEventListener("mouseenter", toggleHover);
-        dropdown[i].addEventListener("mouseleave", closeHover);
-    }
-}
+// initialise menu event listeners based on initial screensize
+if (window.innerWidth < 768)
+    mobileDropdownListeners();
+else
+    desktopDropdownListeners();
